fad273f642faf493bd3bb7a512450789f63f91e2
manikanta <44938140+manikanta55@users.noreply.github.com>
2022-06-01 02:01:53 +0530

    minor module error

--- north-star-ui/src/app/app.module.ts
+++ north-star-ui/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import {MatMenuModule} from '@angular/material/menu';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MenuComponentComponent } from './menu-component/menu-component.component';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
@@ -27,7 +26,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponentComponent,
     FooterComponent,
     AboutNorthStarComponent,
     NothStarCaptionComponent,
